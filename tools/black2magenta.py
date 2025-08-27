a60bf5f38acbefdeac596a5c47d069e17f7e5f39
Jean-François Fabre <jotd666@gmail.com>
2025-08-27 20:11:24 +0200

    almost done with black color for level 3...

---
+++ tools/black2magenta.py
@@ -0,0 +1,62 @@
+from PIL import Image,ImageOps
+import pathlib
+
+# MAME gfx save edition is cool but cannot differentiate background color from black.
+# this is pretty f**ing annoying, fortunately, there's always a CLUT in the set which makes the difference
+# here it's monochrome palette index 0xD that saves us.
+
+this_dir = pathlib.Path(__file__).absolute().parent
+
+magenta = (254,0,254)
+
+def doit(sheet_subdir,fake_black,fake_black_to_apply):
+    sheets_dir = this_dir.parent / "assets" / "sheets" / sheet_subdir
+    src_sprite_dir = sheets_dir / "sprites_black"  # the original sprites ripped from MAME with black background
+    dst_sprite_dir = sheets_dir / "sprites"
+
+    dst_sprite_dir.mkdir(exist_ok=True)
+
+
+
+
+    replacement_pixels = dict()
+
+    for i,fb in fake_black.items():
+        ref_image = Image.open(src_sprite_dir / f"pal_{i:02x}.png")
+        yp = set()
+
+        # this palette has the nice property of having black not merged with background
+        # black colors in other palettes are there yellow, so we can backport the black color in other palettes
+        for x in range(ref_image.size[0]):
+            for y in range(ref_image.size[1]):
+                p = ref_image.getpixel((x,y))
+                if p == fb:
+                    yp.add((x,y))
+        replacement_pixels[i] = {"black_pixels":yp,"ref_image":ref_image}
+        image_size = ref_image.size
+
+    for i,c in enumerate(fake_black_to_apply):
+        imgname = f"pal_{i:02X}.png"
+        src = src_sprite_dir / imgname
+        dst = dst_sprite_dir / imgname
+
+        yp = replacement_pixels[c]["black_pixels"]
+
+        src_image = Image.open(src)
+
+        dst_image = Image.new("RGB",image_size)
+        for x in range(image_size[0]):
+            for y in range(image_size[1]):
+                p = src_image.getpixel((x,y))
+                if p == (0,0,0):
+                    # black: what do do?
+                    if (x,y) in yp:
+                        pass
+                    else:
+                        p = magenta  # black => magenta but not full magenta to avoid conflicts
+                dst_image.putpixel((x,y),p)
+
+        dst_image.save(dst)
+
+doit("level_3",fake_black = {0:(119,102,0),5:(255,0,0)},
+                fake_black_to_apply = [0,0,0,0,0,5,5,0])
