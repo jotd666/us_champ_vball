ac1f295439b46b98be217b39dc55e18749dd0f6d
Jean-François Fabre <jotd666@gmail.com>
2025-11-14 23:29:38 +0100

    added levels 4 & 5 tunes

--- assets/amiga/convert_sounds.py
+++ assets/amiga/convert_sounds.py
@@ -62,6 +62,8 @@ yaah_31""".upper().splitlines()
     sound_dict["demo.mod"] = {"index":5,"volume":32}
     sound_dict["level_1.mod"] = {"index":9,"volume":28}
     sound_dict["level_2.mod"] = {"index":6,"volume":28}
+    sound_dict["level_4.mod"] = {"index":8,"volume":28}
+    sound_dict["level_5.mod"] = {"index":0xD,"volume":28}
     sound_dict["hurry.mod"] = {"index":3,"volume":28}
     sound_dict["won.mod"] = {"index":0x14,"volume":28}
 
